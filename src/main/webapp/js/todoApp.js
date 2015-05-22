06dcf677efef6d45ab321f43fea71125876a4e0c
Yann Massard <yamass@gmail.com>
2015-05-22 14:16:29 +0200

    service

--- src/main/webapp/js/todoApp.js
+++ src/main/webapp/js/todoApp.js
@@ -1,22 +1,42 @@
 var todoModule = angular.module("TodoApp", []);
 
+todoModule.value("myValue", "Hallo myValue");
 
-
-todoModule.controller("TodoController", function() {
+todoModule.controller("TodoController", function(myValue, todoService) {
     var me = this;
 
-    me.todos = [
-        {title: "Haircut", done: false},
-        {title: "Milch und Brot kaufen", done: true}];
+    me.todos = todoService.getAll();
 
     me.newTodo = {};
 
     me.numberOfRemainingTodos = function() {
         return me.todos.filter(function(todo) {return !todo.done;}).length;
-    }
+    };
 
     me.addTodo = function() {
-        me.todos.push(me.newTodo);
-        me.newTodo = {};
+        todoService.add(me.newTodo);
+        me.newTodo = {title : "", done: false};
+        console.log(myValue);
+    }
+});
+
+todoModule.service("todoService", function() {
+
+    var me = this;
+    var idCounter = 1;
+
+    var todos = [
+        {title: "Haircut", done: false},
+        {title: "Milch und Brot kaufen", done: true}
+    ];
+
+    this.getAll =function() {
+        return todos;
+    };
+
+    this.add = function(todo) {
+        todos.push(todo);
+        todo.id = idCounter++;
+        return todo;
     }
 });
